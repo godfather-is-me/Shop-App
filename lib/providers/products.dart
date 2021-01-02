d6d74cccaecdfdf88d04f72bf025b10b6abfa7ce
PrathyushPRao <jay@prashy.com>
2021-01-02 17:58:10 +0530

    adding favorites

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -37,10 +37,29 @@ class Products with ChangeNotifier {
     ),
   ];
 
+  // var _showFavoritesOnly = false;
+
   List<Product> get items {
+    /* if (_showFavoritesOnly) {
+      return _items.where((prodItem) => prodItem.isFavorite).toList();
+    } */
     return [..._items];
     // Returns a copy of the items!!
   }
+
+  List<Product> get favoriteItems {
+    return _items.where((prodItem) => prodItem.isFavorite).toList();
+  }
+
+  /* void showFavoritesOnly() {
+    _showFavoritesOnly = true;
+    notifyListeners();
+  }
+
+  void showAll() {
+    _showFavoritesOnly = false;
+    notifyListeners();
+  } */
 
   Product findById(String id) {
     return _items.firstWhere((element) => element.id == id);
