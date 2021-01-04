aa5aa12f558f90a8213b72d4e4b626cab063c6ee
PrathyushPRao <jay@prashy.com>
2021-01-04 11:34:21 +0530

    order screen

---
+++ lib/widgets/app_drawer.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AppDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          AppBar(
+            title: Text('Hello friend!'),
+            automaticallyImplyLeading: false,
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.shop),
+            title: Text('Shop'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed('/');
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
